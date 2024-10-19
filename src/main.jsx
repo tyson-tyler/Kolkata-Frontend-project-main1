3615bfb7d3fc0f04104036f09bf0e1b8fa055103
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2024-10-19 22:21:41 +0530

    vgvh

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
