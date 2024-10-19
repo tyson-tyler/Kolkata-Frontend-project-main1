6e302a7f28432b6291b5698ab3f394964e2afde3
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2024-10-19 22:17:20 +0530

    ghjhg

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom'
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </StrictMode>
-)
+);
